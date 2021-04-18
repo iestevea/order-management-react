f9c782a983b580ae4bbf7e3c88734a7424db0ba9
iestevea <iciar.ea235@gmail.com>
2021-04-18 21:40:25 +0200

    fix: base api url

--- src/pods/order/api/order.api.ts
+++ src/pods/order/api/order.api.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 import { Order } from "./order.api-model";
 
-const ordersApi = process.env.MOCK_API;
+const ordersApi = process.env.BASE_API_URL;
 
 export const getOrder = async (id: string): Promise<Order> => {
   const { data } = await Axios.get(`${ordersApi}/orders/${id}`);
